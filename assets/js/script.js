272dc889592439266e7a3546a2d3c0464fda29c0
koentjeh <koen-debont@hotmail.com>
2015-01-08 11:53:37 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,107 @@
-    var lollies = 100;
-    var level = 1;
-    var test = 0;
-    
-    $(".ja").html("<h2>klik op de lollie<h2>");
+$( document ).ready(function() {
+	$('.container2').hide();	
 
-function autoclicker (lvl,time) {
-	auto = setInterval(function(){
-			lollies = lollies + lvl;
-			$(".ja").html("<h2>je hebt " + lollies + " mini lollies</h2>");
-	} ,time);
-}
+	var lollies = 1000;
+	var level = 1;
+	var test = 0;
 
-$(".clicker").click(function(){
-	switch(level) {
-    case 1:
-         lollies = lollies + 1;
-		$(".ja").html("<h2>je hebt " + lollies + " mini lollies<h2>");
-        break;
-    case 2:
-        lollies = lollies + 2;
-		$(".ja").html("<h2>je hebt " + lollies + " mini lollies</h2>");
-        break;
+	var upgrade_to_gummy_bear = 0;
+	var gummy_bears = 0;
+	    
+	$(".lollipop_amount").html("<h2>klik op de lollie<h2>");
+	$(".gummybear_amount").html("<h2>klik op de Gummy Bear<h2>");
+
+	function autoclicker (lvl,time) {
+		auto = setInterval(function(){
+				lollies = lollies + lvl;
+				$(".lollipop_amount").html("<h2>je hebt " + lollies + " mini lollies</h2>");
+				$(".testen").html("<p>je hebt " + lollies + " lollies</p>");
+		} ,time);
 	}
-});
 
-$(".keer-2").click(function(){
-	if(level  > 1){
-		
-	}else if(lollies >= 20) {
-		lollies = lollies - 20;
-		level = 2;
-		$(".keer-2").html('<strike>klik keer 2 voor 20 mini lolies</strike>');
-		$(".ja").html("<h2>je hebt " + lollies + " mini lollies</h2>");
-	};
-});
-
-$(".auto-click").click(function(){
-	if (test == 0){
-		if (lollies >= 100){
-			lollies = lollies - 100;
-			$(".ja").html("<h2>je hebt " + lollies + " mini lollies</h2>");
-			$(".auto-click").html("upgrade autoclicker voor 200 mini lollies");
-			autoclicker(1,1000);
-			test = 1
+	$(".lollipop_clicker").click(function(){
+		switch(level) {
+	    case 1:
+	         lollies = lollies + 1;
+			$(".lollipop_amount").html("<h2>je hebt " + lollies + " mini lollies<h2>");
+	        break;
+	    case 2:
+	        lollies = lollies + 2;
+			$(".lollipop_amount").html("<h2>je hebt " + lollies + " mini lollies</h2>");
+	        break;
 		}
-	}
+	});
 
-	if (test == 1){
-			if (lollies >= 200){	
-				lollies = lollies - 200;
-				$(".ja").html("<h2>je hebt " + lollies + " mini lollies</h2>");
-				$(".auto-click").html("<strike>upgrade autoclicker voor 200 mini lollies</strike>");
-				window.clearInterval(auto);
-				autoclicker(2,1000);
-				test = 2
+	$(".keer-2").click(function(){
+		if(level  > 1){
+			
+		}else if(lollies >= 20) {
+			lollies = lollies - 20;
+			level = 2;
+			$(".keer-2").html('<strike>klik keer 2 voor 20 mini lolies</strike>');
+			$(".lollipop_amount").html("<h2>je hebt " + lollies + " mini lollies</h2>");
+		};
+	});
+
+	$(".auto-click").click(function(){
+		if (test == 0){
+			if (lollies >= 100){
+				lollies = lollies - 100;
+				$(".lollipop_amount").html("<h2>je hebt " + lollies + " mini lollies</h2>");
+				$(".auto-click").html("upgrade autoclicker voor 200 mini lollies");
+				autoclicker(1,1000);
+				test = 1
+			}
 		}
-	}
+
+		if (test == 1){
+				if (lollies >= 200){	
+					lollies = lollies - 200;
+					$(".lollipop_amount").html("<h2>je hebt " + lollies + " mini lollies</h2>");
+					$(".auto-click").html("<strike>upgrade autoclicker voor 200 mini lollies</strike>");
+					window.clearInterval(auto);
+					autoclicker(2,1000);
+					test = 2
+			}
+		}
+	});
+
+	$(".upgrade_to_gummy_bear").click(function() {
+		if (upgrade_to_gummy_bear == 0) {
+			if (lollies >= 300) {
+				lollies = lollies - 300;
+				$(".lollipop_amount").html("<h2>je hebt " + gummy_bears + " Gummy Bears</h2>");
+				$(".upgrade_to_gummy_bear").html("Back to Gummy Bears");
+				$('.container').hide();
+				$('.container2').show();
+				upgrade_to_gummy_bear = 1;
+			}
+		} else if (upgrade_to_gummy_bear == 1) {
+				$('.container').hide();
+				$('.container2').show();
+			$(".back_to_lollies").click(function(){
+				$(".container2").hide();
+				$(".container").show();
+			});
+		}
+	});
+
+	$(".gummmybear_clicker").click(function(){
+		switch(level) {
+	    case 1:
+	        gummy_bears = gummy_bears + 1;
+			$(".gummybear_amount").html("<h2>je hebt " + gummy_bears + " Gummy Bears</h2>");
+	        break;
+	    case 2:
+	        gummy_bears = gummy_bears + 2;
+			$(".gummybear_amount").html("<h2>je hebt " + gummy_bears + " Gummy Bears</h2>");
+	        break;
+		}
+	});
+
+	$(".back_to_lollies").click(function(){
+		$(".container2").hide();
+		$(".container").show();
+	});
+	
 });
-
-// $(".upgrade-click").click(function(){
-
-// 	window.clearInterval(auto);
-// 	autoclicker(2,1000);
-// });
--- index.php
+++ index.php
@@ -8,15 +8,48 @@
 </div>
 
 <div class="container">
-		<div class="col-md-3 col-xs-12">
-		<div class="clicker">
+	<div class="col-md-3 col-xs-12">
+		<div class="lollipop_clicker">
 			<img class="lollipop-img" src="assets/img/lollipop2.png" alt="">
 		</div>
 	</div>
 
 	<div class="col-md-5 col-xs-12">
 		<div class="col-md-12">
-			<p class="ja"></p>
+			<p class="lollipop_amount"></p>
+		</div>
+		<div class="col-md-12">
+			<button class="keer-2 btn">klik keer 2 voor 20 mini lolies</button>	
+		</div>
+
+		<div class="col-md-12">
+			<button class="auto-click btn">auto click voor 100 mini lollies</button>
+		</div>
+
+		<div class="col-md-12">
+			<button class="upgrade_to_gummy_bear btn">Upgrade to Gummy Bear</button>
+		</div>
+
+	</div>
+
+	<div class="col-md-4 col-xs-12">
+		<p class="test">test</p>
+		<p>hier komt informatie</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis ab ducimus debitis officiis, magni cumque illum possimus. Ad iure quibusdam, nulla, accusamus deleniti excepturi voluptatem. Praesentium sapiente, libero quas perferendis.</p>
+	</div>
+
+</div>
+
+<div class="container2">
+	<div class="col-md-3 col-xs-12">
+		<div class="gummmybear_clicker">
+			<img class="lollipop-img" src="assets/img/gummy_bear1.png" alt="">
+		</div>
+	</div>
+
+	<div class="col-md-5 col-xs-12">
+		<div class="col-md-12">
+			<p class="gummybear_amount"></p>
 		</div>
 		<div class="col-md-12">
 			<button class="keer-2 btn">klik keer 2 voor 20 mini lolies</button>	
@@ -29,14 +62,10 @@
 	</div>
 
 	<div class="col-md-4 col-xs-12">
-		<p class="test">test</p>
-		<p>hier komt informatie</p>
+		<p class="testen"></p>
+		<p><button class="back_to_lollies btn">Back to lollies</button></p>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis ab ducimus debitis officiis, magni cumque illum possimus. Ad iure quibusdam, nulla, accusamus deleniti excepturi voluptatem. Praesentium sapiente, libero quas perferendis.</p>
 	</div>
-
-
-
-
 </div>
 
 <?php require 'includes/footer.php'; ?>
